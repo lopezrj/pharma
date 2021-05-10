1c7ed48d0b956d7419c3400340136de0721f5e03
lopezrj <lopezrj@hotmail.com>
2021-05-09 18:52:21 -0600

    Pagination

--- routes/farmacos.js
+++ routes/farmacos.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const schema = require("../graphql/schema/schema")
 const {graphql} =  require('graphql')
-const paginate = require('express-paginate');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let page = req.query.page || 1
-  const { limit = 10 } = req.query;
+  const { limit = 20 } = req.query;
   let first = (page-1)* limit
   
   // get total documents in the Posts collection 
@@ -15,11 +14,10 @@ router.get('/', function(req, res, next) {
   let query = `{ queryCountFarmacos, queryPageFarmacos(first:${first}, limit: ${limit}) { id, name, type, drugbank_id, atc_code }}`;
   graphql(schema, query)
   .then(function (data) {
-    let a= paginate.href(req)
     let countDocuments = data.data["queryCountFarmacos"]
     let totalPages = Math.ceil(countDocuments / limit)
 //    res.json(data.data );
-    res.render('farmacos/index', {paginate: a, farmacos: data.data["queryPageFarmacos"], title: 'Farmacos', totalPages: totalPages , currentPage: page })
+    res.render('farmacos/index', {farmacos: data.data["queryPageFarmacos"], title: 'Farmacos', totalPages: totalPages , currentPage: page })
   })
 })
 
